b95f26eba911e253d9c0cae42edb0ba4986a6f0f
kacemws <berras.belkacem@gmail.com>
2020-12-30 19:57:04 +0100

    init boilerplate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./Style/App.scss";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <div
+              style={{
+                height: "100vh",
+                width: "100%",
+                backgroundColor: "red",
+              }}
+            ></div>
+          </Route>
+          <Route path="/sign-up">
+            <div
+              style={{
+                height: "100vh",
+                width: "100%",
+                backgroundColor: "blue",
+              }}
+            ></div>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
