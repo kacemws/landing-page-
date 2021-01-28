8c2c2394a17760f7e7ac35f4495bcf02c7a475cb
kacemws <berras.belkacem@gmail.com>
2021-01-28 23:05:30 +0100

    adding a phone confirmation panel

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React, { Suspense } from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./Style/App.scss";
-const Signin = React.lazy(() => import("./Views/Signin"));
-const Signup = React.lazy(() => import("./Views/Signup"));
+import CodeConfirmation from "./Views/CodeConfirmation";
+import Signin from "./Views/Signin";
+import Signup from "./Views/Signup";
 function App() {
   return (
     <div className="App">
       <Router>
-        <Suspense fallback={<h1>Loading</h1>}>
-          <Switch>
-            <Route exact path="/">
-              <Signin />
-            </Route>
-            <Route path="/sign-up">
-              <Signup />
-            </Route>
-          </Switch>
-        </Suspense>
+        <Switch>
+          <Route exact path="/">
+            <Signin />
+          </Route>
+          <Route path="/sign-up">
+            <Signup />
+          </Route>
+          <Route path="/phone-confirmation">
+            <CodeConfirmation />
+          </Route>
+        </Switch>
       </Router>
     </div>
   );
---
+++ src/Views/CodeConfirmation.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../Style/Views/CodeConfirmation.scss";
+import phone from "../Assets/phone-code.webp";
+
+function CodeConfirmation() {
+  return (
+    <div className="code-confirmation">
+      <div className="card">
+        <div className="container">
+          <div className="header"></div>
+          <div className="content"></div>
+          <div className="footer"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CodeConfirmation;
