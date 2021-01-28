55fea4e23ea98fcca3ba021e7be60035d5daecda
kacemws <berras.belkacem@gmail.com>
2021-01-28 23:37:50 +0100

    added the code input section

--- src/Views/CodeConfirmation.js
+++ src/Views/CodeConfirmation.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../Style/Views/CodeConfirmation.scss";
 import phone from "../Assets/phone-code.webp";
+import retry from "../Assets/resend.svg";
 
 function CodeConfirmation() {
   return (
@@ -10,7 +11,17 @@ function CodeConfirmation() {
           <div className="header">
             <img src={phone} />
           </div>
-          <div className="content"></div>
+          <div className="content">
+            <div className="input">
+              <input type="tel" name="verification-code" />
+            </div>
+            <div className="re-send">
+              <div className="icon">
+                <img src={retry} />
+              </div>
+              <div className="text">Envoyer de nouveau</div>
+            </div>
+          </div>
           <div className="footer"></div>
         </div>
       </div>
